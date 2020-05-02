54a14fd0230a5106f8700188a7ccacc1e592e89a
vpc <cuevasvp@gmail.com>
2020-05-02 11:06:03 +0800

    refactor

--- FirstLastOccurence.py
+++ FirstLastOccurence.py
@@ -1,36 +1,53 @@
 # Find the first occurence of n in a sorted array
 def first_occurence(arr, n):
-    left = 0
-    right = int(len(arr) - 1)
-    mid = int((left + right) / 2)
+    lo = 0
+    hi = len(arr) - 1
 
-    while left <= right:
+    while lo <= hi:
+        mid = (lo + hi) // 2
         if n <= arr[mid]:
-            right = mid - 1
+            hi = mid - 1
         else:
-            left = mid + 1
-        mid = int((left + right) / 2)
-    return left
+            lo = mid + 1
+    return lo
 
 
-# Find the last occurence of n in an array
+# Find the last occurence of n in a sorted array
 def last_occurence(arr, n):
-    left = 0
-    right = int(len(arr) - 1)
-    mid = int((left + right) / 2)
+    lo = 0
+    hi = len(arr) - 1
 
-    while left <= right:
+    while lo <= hi:
+        mid = (lo + hi) // 2
         if n < arr[mid]:
-            right = mid - 1
+            hi = mid - 1
         else:
-            left = mid + 1
-        mid = int((left + right) / 2)
-    return right
+            lo = mid + 1
+    return hi
 
 
-# arr = [1, 2, 3, 4, 5]
+arr = [1, 2, 3, 4, 5]
+assert first_occurence(arr, 1) == 0
+assert first_occurence(arr, 2) == 1
+assert first_occurence(arr, 3) == 2
+assert first_occurence(arr, 4) == 3
+assert first_occurence(arr, 5) == 4
+
+arr = [3, 3, 3, 4, 5, 6]
+assert first_occurence(arr, 3) == 0
+assert last_occurence(arr, 3) == 2
+assert last_occurence(arr, 4) == 3
+assert last_occurence(arr, 5) == 4
+assert last_occurence(arr, 6) == 5
+
+arr = [1, 2, 3, 3, 3]
+assert first_occurence(arr, 3) == 2
+assert last_occurence(arr, 3) == 4
+assert first_occurence(arr, 1) == 0
+assert last_occurence(arr, 1) == 0
+assert first_occurence(arr, 2) == 1
+assert last_occurence(arr, 2) == 1
+
 arr = [1, 2, 3, 3, 3, 4, 5]
-# arr = [3, 3, 3, 4, 5, 6]
-# arr = [1, 2, 3, 3, 3]
-print(first_occurence(arr, 3))
-print(last_occurence(arr, 3))
+assert first_occurence(arr, 3) == 2
+assert last_occurence(arr, 3) == 4
